e86dd46b9614301f6aabb0eb8c975f486f93c24e
zh375162873 <375162873@qq.com>
2016-02-15 15:44:14 +0800

    新增功能

---
+++ shopddt3/Wapalipay/notify_url.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * 通用通知接口demo
+ * ====================================================
+ * 支付完成后，微信会把相关支付和用户信息发送到商户设定的通知URL，
+ * 商户接收回调信息后，根据需要设定相应的处理流程。
+ * 
+ * 这里举例使用log文件形式记录回调信息。
+*/
+include_once("./log_.php");
+//include_once("../ThinkPHP/Library/Vendor/WxPayPubHelper/WxPayPubHelper.php");
+	$log_ = new Log_();
+	$log_name="./notify_url.log";//log文件路径
+	$log_->log_result($log_name,"【异步回调成功参数】:\n".json_encode($_REQUEST)."\n");
+
+/**
+ * 发送HTTP请求方法
+ * @param  string $url 请求URL
+ * @param  array $params 请求参数
+ * @param  string $method 请求方法GET/POST
+ * @return array  $data   响应数据
+ */
+function curl_http($url, $params, $method = 'GET', $header = array(), $multi = false)
+{
+    $opts = array(
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_HTTPHEADER => $header
+    );
+
+    /* 根据请求类型设置特定参数 */
+    switch (strtoupper($method)) {
+        case 'GET':
+            $opts[CURLOPT_URL] = $url . '?' . http_build_query($params);
+            break;
+        case 'POST':
+            //判断是否传输文件
+            $params = $multi ? $params : http_build_query($params);
+            $opts[CURLOPT_URL] = $url;
+            $opts[CURLOPT_POST] = 1;
+            $opts[CURLOPT_POSTFIELDS] = $params;
+            break;
+        default:
+            throw new Exception('不支持的请求方式！');
+    }
+    /* 初始化并执行curl请求 */
+    $ch = curl_init();
+    curl_setopt_array($ch, $opts);
+    $data = curl_exec($ch);
+    $error = curl_error($ch);
+    curl_close($ch);
+    if ($error) throw new Exception('请求发生错误：' . $error);
+    return $data;
+}
+        
+?>
